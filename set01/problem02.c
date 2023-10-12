0e582847828ea6d5dbb164e5f971fbe5e50048f8
Shafinaz Ahsan <shafinazahsan@Shafinazs-MacBook-Air.local>
2023-10-12 16:01:06 +0530

    committed 12/10/2023, 16:01:06

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,10 @@
 //2. Write a C program to add two numbers.
-
+#include<stdio.h>
+int main(void){
+    int a, b, sum;
+    printf("Enter the numbers : ");
+    scanf("%d%d", &a, &b);
+    sum = a + b;
+    printf("Sum : %d\n", sum);
+    return 0;
+}
