235c18f0de1e1721840ecde2e542932277a8fe80
Shafinaz Ahsan <shafinazahsan@Shafinazs-MacBook-Air.local>
2023-10-18 16:19:06 +0530

    committed 18/10/2023, 16:19:06

---
+++ set02/problem03.c
@@ -0,0 +1,37 @@
+/*Write a program to find whether a number is a composite number*/
+#include<stdio.h>
+int input_number(){
+    int n;
+    printf("Enter the number : ");
+    scanf("%d", &n);
+    return n;
+}
+int is_composite(int n){
+    if(n<=1){
+        return 0;
+    }
+    for(int i=2;i<=n/2;i++){
+        if(n%i==0){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+    return 0;
+}
+void output(int n,int result){
+    if(result){
+        printf("%d is a composite number.", n);
+    }
+    else{
+        printf("%d is not a composite bumber.",n);
+    }
+}
+int main(){
+    int n, result;
+    n=input_number();
+    result=is_composite(n);
+    output(n,result);
+    return 0;
+}
