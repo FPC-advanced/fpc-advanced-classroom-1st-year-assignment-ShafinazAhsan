c2e86f83947dfafb649cb08a0ccbacfeb63ada6a
Shafinaz Ahsan <shafinazahsan@Shafinazs-MacBook-Air.local>
2023-12-02 22:51:19 +0530

    committed 02/12/2023, 22:51:19

--- set04/problem08.c
+++ set04/problem08.c
@@ -1,26 +1,26 @@
 /*Write a program to add n fractions*/
 #include <stdio.h>
-typedef struct fraction {
+typedef struct fraction{
     int num, den;
 } Fraction;
-int input_n() {
+int input_n(){
     int n;
     printf("Enter the number of fractions: ");
     scanf("%d", &n);
     return n;
 }
-Fraction input_fraction() {
+Fraction input_fraction(){
     Fraction f;
     printf("Enter the fraction (numerator denominator): ");
     scanf("%d %d", &f.num, &f.den);
     return f;
 }
-void input_n_fractions(int n, Fraction f[n]) {
+void input_n_fractions(int n, Fraction f[n]){
     for (int i = 0; i < n; i++) {
         f[i] = input_fraction();
     }
 }
-int find_gcd(int a, int b) {
+int find_gcd(int a, int b){
     while (b != 0) {
         int c = b;
         b = a % b;
@@ -28,7 +28,7 @@ int find_gcd(int a, int b) {
     }
     return a;
 }
-Fraction add_fractions(Fraction f1, Fraction f2) {
+Fraction add_fractions(Fraction f1, Fraction f2){
     Fraction sum;
     sum.num = f1.num * f2.den + f2.num * f1.den;
     sum.den = f1.den * f2.den;
@@ -37,21 +37,21 @@ Fraction add_fractions(Fraction f1, Fraction f2) {
     sum.num = sum.den / gcd;
     return sum;
 }
-Fraction add_n_fractions(int n, Fraction f[n]) {
+Fraction add_n_fractions(int n, Fraction f[n]){
     Fraction result = f[0];
     for (int i = 1; i < n; i++) {
         result = add_fractions(result, f[i]);
     }
     return result;
 }
-void output(int n, Fraction f[n], Fraction sum) {
+void output(int n, Fraction f[n], Fraction sum){
     printf("%d/%d", f[0].num, f[0].den);
     for (int i = 1; i < n; i++) {
         printf(" + %d/%d", f[i].num, f[i].den);
     }
     printf(" = %d/%d\n", sum.num, sum.den);
 }
-int main() {
+int main(){
     int n = input_n();
     Fraction fractions[n];
     input_n_fractions(n, fractions);
